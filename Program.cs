1d77f6dde338376e1c0b90f67c0507e722e641f2
Auke Swart <auke@swartdev.com>
2019-06-03 12:23:29 +0200

    Test2
    test2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LifeCycleImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Test!");
+        }
+    }
+}
